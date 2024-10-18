d9e2539764bd5a01b1b9fb6b53c2fb75202735e0
RuanCarlosSilva <143547755+ruansilva123@users.noreply.github.com>
2024-10-18 14:20:24 -0300

    :sparkles: feat: criando projeto java

---
+++ Backend/FootbAPI/src/main/java/com/soccerapi/footbapi/FootbApiApplication.java
@@ -0,0 +1,13 @@
+package com.soccerapi.footbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootbApiApplication.class, args);
+	}
+
+}
---
+++ Backend/FootbAPI/src/test/java/com/soccerapi/footbapi/FootbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soccerapi.footbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
