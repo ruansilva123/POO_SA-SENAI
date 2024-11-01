14beb18550b0c2e995902b9661c9314a386cf9af
RuanCarlosSilva <143547755+ruansilva123@users.noreply.github.com>
2024-10-31 21:34:20 -0300

    :arrow_up: deps: adicionando dependências e configurções básicas do projeto

--- api/FootbAPI/src/main/java/com/soccerapi/footbapi/FootbApiApplication.java
+++ api/FootbAPI/src/main/java/com/soccerapi/footbapi/FootbApiApplication.java
@@ -2,12 +2,19 @@ package com.soccerapi.footbapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FootbApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FootbApiApplication.class, args);
 	}
 
+	@GetMapping("teste/")
+	public String teste(){
+		return "Teste2";
+	}
 }
